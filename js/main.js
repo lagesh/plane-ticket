d63982154e5346c4a73f35dab6029ff95b118e2b
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-15 23:36:57 +0000

    initial Js declarations

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let passengerName1 = document.querySelector('.name');
+let passengerName2 = document.querySelector('.stub-name');
+console.log(passengerName2, passengerName1);
+
+let passengerName = [passengerName1, passengerName2];
+passengerName.forEach(name => {
+  name.style.visibility = 'hidden';
+})
