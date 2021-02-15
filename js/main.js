cc1433385edd22af106539e1e163e5aa2dbbfa4b
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-15 23:49:13 +0000

    edit flight details

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let passengerName1 = document.querySelector('.name');
 let passengerName2 = document.querySelector('.stub-name');
-console.log(passengerName2, passengerName1);
+// console.log(passengerName2, passengerName1);
 
 let passengerName = [passengerName1, passengerName2];
 passengerName.forEach(name => {
