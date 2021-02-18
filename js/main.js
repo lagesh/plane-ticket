24d90f8d4f215f17c366ba84f571fee973f52a34
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-18 00:09:10 +0000

    refactor js

--- js/main.js
+++ js/main.js
@@ -2,26 +2,28 @@ let passengerName1 = document.querySelector('.name');
 let passengerName2 = document.querySelector('.stub-name');
 
 let passengerName = [passengerName1, passengerName2];
+/*
 passengerName.forEach(name => {
   name.style.visibility = 'hidden';
 });
-
+*/
 let passengerInput = document.getElementById('name');
 let input = document.querySelector('input');
 
-function refresh() {
+/*function refresh() {
   let passName = document.getElementsByClassName('passenger');
   passName.style.margin = "15px 20px"
 }
-
+*/
 function inputValue() {
   passengerName.forEach(name => {
-    name.style.visibility = 'visible';
+    // name.style.visibility = 'visible';
     name.innerHTML = input.value.toUpperCase();
     if (input.value.length === 0) {
-      return refresh;
+      name.innerHTML = '---';
     }
   });
+
 }
 passengerInput.oninput = inputValue;
 
